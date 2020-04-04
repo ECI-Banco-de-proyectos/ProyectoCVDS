ceadfa2764a1016648d1be6b25c3407cd2af4dc8
Sebastian Nieto <juan.nieto-mo@mail.escuelaing.edu.co>
2020-04-04 18:41:09 -0500

    base de datos

---
+++ src/main/java/edu/eci/cvds/samples/services/ServiceXFactory.java
@@ -0,0 +1,18 @@
+/**package edu.eci.cvds.samples.services;
+
+public class ServiceXFactory {
+
+	private static ServiceXFactory instance =new ServiceXFactory();
+	private static Injector injector;
+	private static Injector testInector;
+	
+	public ServiceXFactory() {
+		injector = createInjector(new XMLMyBatisModule() {
+			
+			
+		}
+	}
+
+}
+
+**/
